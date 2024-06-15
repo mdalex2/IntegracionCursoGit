411428fa7e9db9058145d67c11fc7507a22d908d
Alexi Mendoza <mdalex2@gmail.com>
2024-06-15 13:02:07 -0300

    Cambiar nombre a método

--- API/Controllers/ProductoController.cs
+++ API/Controllers/ProductoController.cs
@@ -13,7 +13,7 @@ namespace API.Controllers
     public class ProductoController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<List<ProductoDto>> Get()
+        public ActionResult<List<ProductoDto>> GetProductos()
         {
             return ProductoData.listaProductos
             .Select(s => new ProductoDto{
