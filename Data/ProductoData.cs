1f3a49db3bc51c40078c60dfd248a52d78977420
Alexi Mendoza <mdalex2@gmail.com>
2024-06-15 12:10:59 -0300

    Proyecto Inicial

---
+++ Data/ProductoData.cs
@@ -0,0 +1,13 @@
+﻿using Models.Entidades;
+
+namespace Data;
+
+public static class ProductoData
+{
+    public static List<Producto> listaProductos = new(){
+        new Producto{ Id = 1, NombreProducto = "Producto 01",Marca = "HP", Categoria = "Computadoras", Precio = 2000},
+        new Producto{ Id = 1, NombreProducto = "Producto 02",Marca = "HP", Categoria = "Computadoras", Precio = 2200},
+        new Producto{ Id = 1, NombreProducto = "Producto 03",Marca = "HP", Categoria = "Computadoras", Precio = 2350},
+        new Producto{ Id = 1, NombreProducto = "Producto 04",Marca = "HP", Categoria = "Computadoras", Precio = 2450}
+    };
+}
